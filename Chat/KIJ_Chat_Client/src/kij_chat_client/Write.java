680264b328059da0a645355fe66fa4c065068ac6
Muhammad Faiez Ananda <faiez1995@gmail.com>
2016-04-27 12:27:50 +0700

    Update group message encryption with AES ECB

--- Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
+++ Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.xml.bind.DatatypeConverter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -109,7 +110,7 @@ public class Write implements Runnable {
 //                                            System.out.println(input);
                                             out.println(command + " " + key + " " + encryptedValue);//SEND IT TO THE SERVER             
                                 }
-                                if (input.split(" ")[0].toLowerCase().equals("pm") == true)
+                                else if (input.split(" ")[0].toLowerCase().equals("pm") == true)
                                 {
                                     String huruf="";
                                      String[] pisah = input.split(" ");
@@ -119,7 +120,7 @@ public class Write implements Runnable {
                                      //enkrip(huruf);
                                    System.out.println(huruf);
                                      
-                                   Date date = new Date();
+                                     Date date = new Date();
                                      DateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
                                      String tanggal = dateFormat.format(date);
                                      //System.out.println("Date converted to String: " + strDate);
@@ -136,10 +137,46 @@ public class Write implements Runnable {
                                      String encoded = new BASE64Encoder().encode(cipherText);
                                      input = pisah[0] + " " + pisah[1] + " " + encoded;
                                 }
-				out.println(input);//SEND IT TO THE SERVER
-				out.flush();//FLUSH THE STREAM
                                 
-                                
+                                else if(input.split(" ")[0].toLowerCase().equals("gm") == true){
+                                    String[] vals=input.split(" ");
+                                    command = vals[0];
+                                        //String message = vals[1];
+                                        //message = String.format("%-16s", vals[1]).replace(' ', '0');
+                                        String messageOut = "";
+                                        for (int j = 2; j<vals.length; j++) {
+                                            messageOut += vals[j] + " ";
+                                        }
+                                        byte[] plaintext = messageOut.getBytes();
+                                        Date date = new Date();
+                                        DateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
+                                        String tanggal = dateFormat.format(date);
+                                        String key_coy = String.format("%-16s",tanggal).replace(' ','0');
+                                        
+                                        //System.out.println("Date converted to String: " + strDate);
+                                        byte [] key1 = key_coy.getBytes();
+
+                                        SecretKeySpec skeySpec=new SecretKeySpec(key1,"AES");
+                                        Cipher AESCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                                        AESCipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+                                        byte[] ciphertext = AESCipher.doFinal(plaintext); 
+                                        encryptedValue = new BASE64Encoder().encode(ciphertext);
+                                        //input = vals[0] + " " + vals[1] + " " + ciphertext ;
+                                        //Socket s = new Socket("localhost", 7777);
+                                        //DataOutputStream dOut = new DataOutputStream(this.socket.getOutputStream());
+                                        //dOut.writeInt(iv.length);
+                                        //dOut.write(iv);
+//                                            System.out.println(key_fix);
+//                                            System.out.println(iv);
+//                                            System.out.println(skeySpec);
+//                                            System.out.println(ivSpec);
+//                                            System.out.println(input);
+                                        out.println(command + " " + vals[1] + " " +encryptedValue);//SEND IT TO THE SERVER
+                                }
+                                else{
+                                    command = input;
+                                    out.println(command);//SEND IT TO THE SERVER
+                                }
                                 
                                 if (input.contains("logout")) {
                                     if (log.contains("true"))
