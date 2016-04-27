1f0e7d3ad4e5b424c135fb972958ae6a2ce7aafa
Bagas Andita Setiawan <tugastok7@gmail.com>
2016-04-27 14:47:13 +0700

    commit bareng fix bug

--- Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
+++ Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
@@ -5,19 +5,21 @@
  */
 package kij_chat_client;
 
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.spec.IvParameterSpec;
-import javax.xml.bind.DatatypeConverter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
+//import java.util.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import sun.misc.BASE64Encoder;
 
 /**
@@ -25,54 +27,32 @@ import sun.misc.BASE64Encoder;
  * @author santen-suru
  */
 public class Write implements Runnable {
-    
+        private Socket socket;//SOCKET INSTANCE VARIABLE
 	private Scanner chat;
         private PrintWriter out;
         boolean keepGoing = true;
+        boolean login = false;
         ArrayList<String> log;
 	
-	public Write(Scanner chat, PrintWriter out, ArrayList<String> log)
+	public Write(Socket s, Scanner chat, PrintWriter out, ArrayList<String> log)
 	{
+                socket = s;
 		this.chat = chat;
                 this.out = out;
                 this.log = log;
 	}
-/*public static void enkrip(String args) throws Exception
-   {
-       Date date = new Date();
-       DateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
-       String tanggal = dateFormat.format(date);
-       //System.out.println("Date converted to String: " + strDate);
-        byte [] key = tanggal.getBytes();
-
-      String clearText = args;
-
-      Cipher rc4 = Cipher.getInstance("RC4");
-      SecretKeySpec rc4Key = new SecretKeySpec(key, "RC4");
-      rc4.init(Cipher.ENCRYPT_MODE, rc4Key);
-
-      byte [] cipherText = rc4.update(clearText.getBytes("ASCII"));
-
-      System.out.println("clear (ascii)        " + clearText);
-      System.out.println("clear (hex)          " + DatatypeConverter.printHexBinary(clearText.getBytes()));
-      System.out.println("cipher (hex) is      " + DatatypeConverter.printHexBinary(cipherText));
-
-      Cipher rc4Decrypt = Cipher.getInstance("RC4");
-      rc4Decrypt.init(Cipher.DECRYPT_MODE, rc4Key);
-      byte [] clearText2 = rc4Decrypt.update(cipherText);
-
-      System.out.println("decrypted (clear) is " + new String(clearText2, "ASCII"));
-   }
-*/	
+	
 	@Override
 	public void run()//INHERIT THE RUN METHOD FROM THE Runnable INTERFACE
 	{
+            
 		try
 		{
 			while (keepGoing)//WHILE THE PROGRAM IS RUNNING
-			{						
+			{	
 				String input = chat.nextLine();	//SET NEW VARIABLE input TO THE VALUE OF WHAT THE CLIENT TYPED IN
-				String encryptedValue =null;
+                                //byte[] iv = null;
+                                String encryptedValue =null;
                                 String key=null;
                                 String command=null;
                                 
@@ -83,12 +63,12 @@ public class Write implements Runnable {
                                             key = vals[1];      
                                             byte[] plaintext = plain_pass.getBytes("UTF-8");
                                             
-                                            String keyData = String.format("%-16s",key).replace(' ','0'); //username+ zeropadding 16 char
+                                            String keyData = String.format("%-16s",key).replace(' ','0');
                                             byte[] key_fix = keyData.getBytes("UTF-8");
                                             
                                             String plain_IV = new StringBuffer(key).reverse().toString();
                                             //System.out.print(plain_IV);
-                                            String ivData = String.format("%-16s",plain_IV).replace(' ','0');//reverse username + zeropadding 16char
+                                            String ivData = String.format("%-16s",plain_IV).replace(' ','0');
                                             byte[] iv = ivData.getBytes("UTF-8");
                                            
                                             
@@ -97,7 +77,7 @@ public class Write implements Runnable {
                                             Cipher AESCipher = Cipher.getInstance("AES/CFB/NoPadding");
                                             AESCipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivSpec);
                                             byte[] ciphertext = AESCipher.doFinal(plaintext); 
-                                            encryptedValue = new BASE64Encoder().encode(ciphertext); //have to do this to save the byte[]
+                                            encryptedValue = new BASE64Encoder().encode(ciphertext);
                                             input = vals[0] + " " + vals[1] + " " + ciphertext ;
                                             //Socket s = new Socket("localhost", 7777);
                                             //DataOutputStream dOut = new DataOutputStream(this.socket.getOutputStream());
@@ -110,33 +90,40 @@ public class Write implements Runnable {
 //                                            System.out.println(input);
                                             out.println(command + " " + key + " " + encryptedValue);//SEND IT TO THE SERVER             
                                 }
+                                
+                               
                                 else if (input.split(" ")[0].toLowerCase().equals("pm") == true)
                                 {
-                                    String huruf="";
-                                     String[] pisah = input.split(" ");
-                                     for (int j = 2; j<pisah.length; j++) {
-                                             huruf+= pisah[j] + " "; //ngambil kata-katanya             
-                                         }
-                                     //enkrip(huruf);
-                                   System.out.println(huruf);
-                                     
-                                     Date date = new Date();
-                                     DateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
-                                     String tanggal = dateFormat.format(date);
-                                     //System.out.println("Date converted to String: " + strDate);
-                                     byte [] key = tanggal.getBytes();
-                                     Cipher rc4 = Cipher.getInstance("RC4");
-                                     SecretKeySpec rc4Key = new SecretKeySpec(key, "RC4");
-                                     rc4.init(Cipher.ENCRYPT_MODE, rc4Key);
+                                        String vals[] = input.split(" ");
+                                         command = vals[0];
+                                         String ke_siapa = vals[1];
+                                         String huruf="";
+                                         String[] pisah = input.split(" ");
+                                         for (int j = 2; j<pisah.length; j++) {
+                                                 huruf+= pisah[j] + " "; //ngambil kata-katanya             
+                                             }
+                                         //enkrip(huruf);
+                                        //System.out.println(huruf);
+                                         byte[] plaintext = huruf.getBytes();
+                                         Date date = new Date();
+                                         DateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
+                                         String tanggal = dateFormat.format(date);
+                                         //System.out.println("Date converted to String: " + strDate);
+                                         byte [] key1 = tanggal.getBytes();
+                                         Cipher rc4 = Cipher.getInstance("RC4");
+                                         SecretKeySpec rc4Key = new SecretKeySpec(key1, "RC4");
+                                         rc4.init(Cipher.ENCRYPT_MODE, rc4Key);
 
-                                     byte [] cipherText = rc4.update(huruf.getBytes());
-                                     Cipher rc4Decrypt = Cipher.getInstance("RC4");
-                                     rc4Decrypt.init(Cipher.DECRYPT_MODE, rc4Key);
-                                     byte [] clearText2 = rc4Decrypt.update(cipherText);
-                                     System.out.println("decrypted (clear) is " + new String(clearText2, "ASCII"));
-                                     String encoded = new BASE64Encoder().encode(cipherText);
-                                     input = pisah[0] + " " + pisah[1] + " " + encoded;
+                                         byte [] cipherText = rc4.update(huruf.getBytes());
+                                         Cipher rc4Decrypt = Cipher.getInstance("RC4");
+                                         rc4Decrypt.init(Cipher.DECRYPT_MODE, rc4Key);
+                                         byte [] clearText2 = rc4Decrypt.update(plaintext);
+                                         encryptedValue = new BASE64Encoder().encode(clearText2);
+                                         //System.out.println("decrypted (clear) is " + new String(clearText2, "ASCII"));
+                                         //System.out.println(command + " " + encryptedValue);//SEND IT TO THE SERVER
+                                         out.println(command + " " + ke_siapa+ " " + encryptedValue);//SEND IT TO THE SERVER
                                 }
+                                
                                 else if(input.split(" ")[0].toLowerCase().equals("bm") == true){
                                     String[] vals = input.split(" ");
                                         command = vals[0];
@@ -173,8 +160,6 @@ public class Write implements Runnable {
 //                                            System.out.println(input);
                                         out.println(command + " " + encryptedValue);//SEND IT TO THE SERVER
                                 }
-                                
-                                
                                 else if(input.split(" ")[0].toLowerCase().equals("gm") == true){
                                     String[] vals=input.split(" ");
                                     command = vals[0];
@@ -214,6 +199,13 @@ public class Write implements Runnable {
                                     command = input;
                                     out.println(command);//SEND IT TO THE SERVER
                                 }
+//                                OutputStream outi = socket.getOutputStream();
+//                                DataOutputStream dOut = new DataOutputStream(outi);
+//                                dOut.writeInt(iv.length);
+//                                dOut.write(iv);
+//                                System.out.println(iv);
+				//out.println(command + " " + key + " " + encryptedValue);//SEND IT TO THE SERVER
+				out.flush();//FLUSH THE STREAM
                                 
                                 if (input.contains("logout")) {
                                     if (log.contains("true"))
